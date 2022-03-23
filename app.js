fa1b3b83cfe7067bf9789609d15df1ee84bd4fc8
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-03-23 18:21:54 -0300

    OK

--- app.js
+++ app.js
@@ -11,8 +11,11 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
     var scroll_position = window.scrollY;
     if (scroll_position > 850) {
-        header.style.backgroundColor = "rgba(0, 0, 0)"
+        header.style.backgroundColor = "rgba(0, 0, 0, 0.955)"
         header.style.borderBottom = "1px solid rgba(73, 251, 53, 0.237)";
+    }  else if (scroll_position > 500) {
+        header.style.backgroundColor = "rgba(0, 0, 0, 0.555)"
+        header.style.borderBottom = "1px solid rgba(73, 251, 53, 0.137)";
     }  else if (scroll_position > 250) {
         header.style.backgroundColor = "rgba(255, 255, 255, 0.050)"
         header.style.borderBottom = "0";
